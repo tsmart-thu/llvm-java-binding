11730b2ae0a9d25f5926ae838afedcef8fa2617c
cgcgbcbc <cgcgbcbc@163.com>
2017-02-26 15:37:02 +0800

    basic llvm types

---
+++ IRReader.java
@@ -0,0 +1,19 @@
+package cn.edu.thu.tsmart.core.cfa.llvm;
+
+import org.bytedeco.javacpp.BytePointer;
+
+import static org.bytedeco.javacpp.LLVM.*;
+
+/**
+ * @author guangchen on 24/02/2017.
+ */
+public class IRReader {
+    public Module parseIRFile(String fileName, Context context) {
+        LLVMMemoryBufferRef bufferRef = new LLVMMemoryBufferRef();
+        BytePointer outMessage = new BytePointer(new BytePointer());
+        LLVMCreateMemoryBufferWithContentsOfFile(new BytePointer(fileName), bufferRef, outMessage);
+        LLVMModuleRef moduleRef = new LLVMModuleRef();
+        LLVMParseIRInContext(context.getContextRef(), bufferRef, moduleRef, outMessage);
+        return new Module(moduleRef);
+    }
+}
