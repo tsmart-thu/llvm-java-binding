aee64a6a7b957adbf57a2c03d35cda089ae8b7e9
ZhCh <yuiyinf@gmail.com>
2017-03-26 15:46:22 +0800

    implement extractvalue and insertvalue

--- ExtractValueInst.java
+++ ExtractValueInst.java
@@ -21,15 +21,39 @@ package cn.edu.thu.tsmart.core.cfa.llvm;
 
 import static cn.edu.thu.tsmart.core.cfa.llvm.InstructionProperties.*;
 
+import com.google.common.collect.ImmutableList;
+
 /**
  * @author guangchen on 27/02/2017.
  */
 public class ExtractValueInst extends UnaryInstruction {
 
+  // TODO initialize in Converter
+  private ImmutableList<Integer> indices;
+
   public ExtractValueInst(String name, Type type) {
     super(name, type);
     super.opCode = OpCode.EXTRACTVALUE;
-    // this instruction should not be used when analysing C
-    assert false : "Unhandled instruction: invoke";
+  }
+
+  // only for Converter
+  public void setIndices(ImmutableList<Integer> indices) {
+    this.indices = indices;
+  }
+
+  public Value getAggregateOperand() {
+    return getOperand(0);
+  }
+
+  public boolean hasIndices() {
+    return true;
+  }
+
+  public int getNumIndices() {
+    return indices.size();
+  }
+
+  public ImmutableList<Integer> getIndices() {
+    return indices;
   }
 }
--- InsertValueInst.java
+++ InsertValueInst.java
@@ -21,15 +21,43 @@ package cn.edu.thu.tsmart.core.cfa.llvm;
 
 import static cn.edu.thu.tsmart.core.cfa.llvm.InstructionProperties.*;
 
+import com.google.common.collect.ImmutableList;
+
 /**
  * @author guangchen on 27/02/2017.
  */
 public class InsertValueInst extends Instruction {
 
+  // TODO initialize in Converter
+  private ImmutableList<Integer> indices;
+
   public InsertValueInst(String name, Type type) {
     super(name, type);
     super.opCode = OpCode.INSERTVALUE;
-    // this instruction should not be used when analysing C
-    assert false : "Unhandled instruction: invoke";
+  }
+
+  // only for Converter
+  public void setIndices(ImmutableList<Integer> indices) {
+    this.indices = indices;
+  }
+
+  public Value getAggregateOperand() {
+    return getOperand(0);
+  }
+
+  public Value 	getInsertedValueOperand() {
+    return getOperand(1);
+  }
+
+  public boolean hasIndices() {
+    return true;
+  }
+
+  public int getNumIndices() {
+    return indices.size();
+  }
+
+  public ImmutableList<Integer> getIndices() {
+    return indices;
   }
 }
