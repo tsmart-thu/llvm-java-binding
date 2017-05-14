b634199d0a662a061f55f51878f6c685f662c3bc
cgcgbcbc <cgcgbcbc@163.com>
2017-05-14 14:57:50 +0800

    BinaryOperator op string

--- BinaryOperator.java
+++ BinaryOperator.java
@@ -40,9 +40,45 @@ public class BinaryOperator extends Instruction {
     return visitor.visit(this);
   }
 
+  private String operatorToString() {
+    switch (opCode) {
+      case ADD:
+        return "add";
+      case FADD:
+        return "fadd";
+      case SUB:
+        return "sub";
+      case FSUB:
+        return "fsub";
+      case MUL:
+        return "mul";
+      case FMUL:
+        return "fmul";
+      case UDIV:
+        return "udiv";
+      case UREM:
+        return "urem";
+      case FREM:
+        return "frem";
+      case SHL:
+        return "shl";
+      case ASHR:
+        return "ashr";
+      case LSHR:
+        return "lshr";
+      case AND:
+        return "and";
+      case OR:
+        return "or";
+      case XOR:
+        return "xor";
+    }
+    return "";
+  }
+
   @Override
   public String toString() {
-    String res = "%" + getName() + " = add nsw ";
+    String res = "%" + getName() + " = " + operatorToString()  + " nsw ";
     res +=
         getOperand(0).getType().toString() + " %" + getOperand(0).getName() + ", %" + getOperand(1)
             .getName();
