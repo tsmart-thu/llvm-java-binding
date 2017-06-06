d5ced8b151bd122d4a0485eea0daa57f2f2022eb
ZhCh <yuiyinf@gmail.com>
2017-06-06 19:49:43 +0800

    modify apint and attribute

--- Attribute.java
+++ Attribute.java
@@ -21,6 +21,8 @@ package cn.edu.thu.tsmart.core.cfa.llvm;
 
 import cn.edu.thu.sse.common.util.Pair;
 import com.google.common.base.Optional;
+import com.google.common.primitives.UnsignedInteger;
+import com.google.common.primitives.UnsignedLong;
 import java.util.ArrayList;
 
 /**
@@ -29,12 +31,9 @@ import java.util.ArrayList;
 public class Attribute {
 
   private AttributeKind attributeKind;
-  // NOTICE that type of an integer parameter of attribute in LLVM IR should be UNSIGNED or UINT_64
-  // Unsigned parameter is zero-extended to 64 bits and then stored in a long variable
-  // Uint_64 parameter is stored in a long variable with bits unchanged
-  private long intParam1;
-  private long intParam2 = -1;
-  private String stringParam = "";
+  private UnsignedLong intParam1 = null;
+  private UnsignedLong intParam2 = null;
+  private String stringParam = null;
 
   private static ArrayList<Attribute> instances = new ArrayList<>();
 
@@ -42,12 +41,12 @@ public class Attribute {
     this.attributeKind = attributeKind;
   }
 
-  private Attribute(AttributeKind attributeKind, long param) {
+  private Attribute(AttributeKind attributeKind, UnsignedLong param) {
     this.attributeKind = attributeKind;
     this.intParam1 = param;
   }
 
-  private Attribute(AttributeKind attributeKind, long param1, long param2) {
+  private Attribute(AttributeKind attributeKind, UnsignedLong param1, UnsignedLong param2) {
     this.attributeKind = attributeKind;
     this.intParam1 = param1;
     this.intParam2 = param2;
@@ -77,20 +76,24 @@ public class Attribute {
     return attribute;
   }
 
-  // NOTICE the 2nd parameter type uses long to store unsigned/uint_64 (regarding to attribute kind)
-  public static Attribute getIntAttribute(AttributeKind attributeKind, long param) {
+  public static Attribute getIntAttribute(AttributeKind attributeKind, UnsignedLong param) {
     assert attributeKind == AttributeKind.ALIGN || attributeKind == AttributeKind.ALIGNSTACK
         || attributeKind == AttributeKind.DEREFERENCEABLE
         || attributeKind == AttributeKind.DEREFERENCEABLE_OR_NULL
         || attributeKind == AttributeKind.ALLOCSIZE : "Not int attribute kind!";
     if (attributeKind == AttributeKind.ALIGN || attributeKind == AttributeKind.ALIGNSTACK
         || attributeKind == AttributeKind.ALLOCSIZE) {
-      assert param >= 0 && param < 4294967296L : "Parameter should range in [0, 2^32 - 1]!";
+      assert param.compareTo(UnsignedLong.valueOf(4294967296L))
+          < 0 : "Parameter should range in [0, 2^32 - 1]!";
     }
 
     for (Attribute attribute : instances) {
-      if (attribute.getAttributeKind() == attributeKind && attribute.getValueAsInt() == param) {
-        return attribute;
+      if (attribute.getAttributeKind() == attributeKind
+          && param.compareTo(attribute.getValueAsInt()) == 0) {
+        if (attributeKind != AttributeKind.ALLOCSIZE || !attribute.getAllocSizeArgs().getSecond()
+            .isPresent()) {
+          return attribute;
+        }
       }
     }
 
@@ -99,25 +102,35 @@ public class Attribute {
     return attribute;
   }
 
-  // NOTICE both the 2nd & 3rd parameter type uses long to store unsigned
-  public static Attribute getAllocSizeAttribute(AttributeKind attributeKind, long param1,
-      long param2) {
-    assert attributeKind == AttributeKind.ALLOCSIZE : "Not allocsize attribute kind!";
-    assert param1 >= 0 && param1 < 4294967296L && param2 >= 0
-        && param2 < 4294967296L : "Parameter should range in [0, 2^32 - 1]!";
-
+  public static Attribute getAllocSizeAttribute(AttributeKind attributeKind,
+      Pair<UnsignedInteger, Optional<UnsignedInteger>> params) {
     for (Attribute attribute : instances) {
       if (attribute.getAttributeKind() == attributeKind
-          && attribute.getAllocSizeArgs().getFirst().longValue() == param1 && attribute
-          .getAllocSizeArgs().getSecond().isPresent()
-          && attribute.getAllocSizeArgs().getSecond().get().longValue() == param2) {
-        return attribute;
+          && attribute.getAllocSizeArgs().getFirst().compareTo(params.getFirst()) == 0) {
+        if (!attribute.getAllocSizeArgs().getSecond().isPresent() && !params.getSecond()
+            .isPresent()) {
+          return attribute;
+        } else if (attribute.getAllocSizeArgs().getSecond().isPresent() && params.getSecond()
+            .isPresent()
+            && attribute.getAllocSizeArgs().getSecond().get().compareTo(params.getSecond().get())
+            == 0) {
+          return attribute;
+        }
       }
     }
 
-    Attribute attribute = new Attribute(attributeKind, param1, param2);
-    instances.add(attribute);
-    return attribute;
+    if (params.getSecond().isPresent()) {
+      Attribute attribute = new Attribute(attributeKind,
+          UnsignedLong.valueOf(params.getFirst().toString()),
+          UnsignedLong.valueOf(params.getSecond().get().toString()));
+      instances.add(attribute);
+      return attribute;
+    } else {
+      Attribute attribute = new Attribute(attributeKind,
+          UnsignedLong.valueOf(params.getFirst().toString()));
+      instances.add(attribute);
+      return attribute;
+    }
   }
 
   public static Attribute getStringAttribute(AttributeKind attributeKind, String param) {
@@ -125,7 +138,8 @@ public class Attribute {
         || attributeKind == AttributeKind.THUNK : "Not string attribute kind!";
 
     for (Attribute attribute : instances) {
-      if (attribute.getAttributeKind() == attributeKind && attribute.getValueAsString().equals(param)) {
+      if (attribute.getAttributeKind() == attributeKind && attribute.getValueAsString()
+          .equals(param)) {
         return attribute;
       }
     }
@@ -155,8 +169,7 @@ public class Attribute {
         || attributeKind == AttributeKind.THUNK;
   }
 
-  // NOTICE the return type uses long to store unsigned/uint_64 (regarding to attribute kind)
-  public long getValueAsInt() {
+  public UnsignedLong getValueAsInt() {
     assert isIntAttribute() : "Expected the attribute to be an integer attribute!";
     return intParam1;
   }
@@ -166,41 +179,38 @@ public class Attribute {
     return stringParam;
   }
 
-  // NOTICE the return type uses long to store unsigned
-  public long getAlignment() {
+  public UnsignedInteger getAlignment() {
     assert attributeKind == AttributeKind.ALIGN : "getAlignment() only works for attribute align!";
-    return intParam1;
+    return UnsignedInteger.valueOf(intParam1.toString());
   }
 
-  // NOTICE the return type uses long to store unsigned
-  public long getStackAlignment() {
+  public UnsignedInteger getStackAlignment() {
     assert attributeKind
         == AttributeKind.ALIGNSTACK : "getStackAlignment() only works for attribute alignstack!";
-    return intParam1;
+    return UnsignedInteger.valueOf(intParam1.toString());
   }
 
-  // NOTICE the return type uses long to store u_int64
-  public long getDereferenceableBytes() {
+  public UnsignedLong getDereferenceableBytes() {
     assert attributeKind
         == AttributeKind.DEREFERENCEABLE : "getDereferenceableBytes() only works for attribute dereferenceable!";
     return intParam1;
   }
 
-  // NOTICE the return type uses long to store u_int64
-  public long getDereferenceableOrNullBytes() {
+  public UnsignedLong getDereferenceableOrNullBytes() {
     assert attributeKind
         == AttributeKind.DEREFERENCEABLE_OR_NULL : "getDereferenceableOrNullBytes() only works for attribute dereferenceable_or_null!";
     return intParam1;
   }
 
-  // NOTICE the return type uses long to store unsigned
-  public Pair<Long, Optional<Long>> getAllocSizeArgs() {
+  public Pair<UnsignedInteger, Optional<UnsignedInteger>> getAllocSizeArgs() {
     assert attributeKind
         == AttributeKind.ALLOCSIZE : "getAllocSizeArgs() only works for attribute allocsize!";
-    if (intParam2 >= 0) {
-      return Pair.of(new Long(intParam1), Optional.of(new Long(intParam2)));
+    if (intParam2 != null) {
+      return Pair.of(UnsignedInteger.valueOf(intParam1.toString()),
+          Optional.of(UnsignedInteger.valueOf(intParam2.toString())));
     } else {
-      return Pair.of(new Long(intParam1), Optional.<Long>absent());
+      return Pair
+          .of(UnsignedInteger.valueOf(intParam1.toString()), Optional.<UnsignedInteger>absent());
     }
   }
 
