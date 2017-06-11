081ea56e2900868f9040efb90cbc2f8196d1b5d2
Min Zhou <zhoumin03@gmail.com>
2017-06-11 02:44:29 -0700

    refactor post-dominator-id -> exploration-priority, fix bug in switch

--- SwitchInst.java
+++ SwitchInst.java
@@ -28,6 +28,22 @@ import static cn.edu.thu.tsmart.core.cfa.util.Casting.cast;
 import static cn.edu.thu.tsmart.core.cfa.util.Casting.dyncast;
 
 /**
+ * operands of switch are arranged as follows (assume n cases)
+ * 0: the value on which the decision is made
+ * 1: the default destination
+ * 2: the value labeled on the 1st case
+ * 3: the destination of the 1st case
+ * ...
+ * 2n: the value labeled on the n-th case
+ * 2n+1: the destination of the n-th case
+ *
+ * Assume the total number of operands is m (=2n+2), the following holds:
+ * 1. The switch instructions has (m/2)-1 cases
+ * 2. The value labeled on the i-th case (count from 0) is indexed as 2(i+1)
+ * 3. The destination of the i-th case (count from 0) is indexed as 2(i+1)+1
+ *
+ * -- Note by zhoumin, which needs to be confirmed.
+ *
  * @author guangchen on 27/02/2017.
  */
 public class SwitchInst extends TerminatorInst {
@@ -37,7 +53,7 @@ public class SwitchInst extends TerminatorInst {
     super.opCode = OpCode.SWITCH;
   }
 
-  public Value getCondition() {
+  public Value getDecisionValue() {
     return getOperand(0);
   }
 
@@ -47,6 +63,24 @@ public class SwitchInst extends TerminatorInst {
 
   public int getNumCases() {
     return getNumOperands() / 2 - 1;
+  }
+
+  /**
+   * return the i-th (count from 0) value that corresponds to the case label
+   * @param i
+   * @return
+   */
+  public ConstantInt getCaseValue(int i) {
+    return dyncast(getOperand((i + 1) * 2), ConstantInt.class);
+  }
+
+  /**
+   * return the i-th (count from 0) destination basic block
+   * @param i
+   * @return
+   */
+  public BasicBlock getCaseDest(int i) {
+    return cast(getOperand((i + 1) * 2 + 1), BasicBlock.class);
   }
 
   @Override
@@ -97,8 +131,8 @@ public class SwitchInst extends TerminatorInst {
   @Override
   public String toString() {
     String res = "switch ";
-    res += getCondition().getType().toString() + " ";
-    res += Formatter.asOperand(getCondition());
+    res += getDecisionValue().getType().toString() + " ";
+    res += Formatter.asOperand(getDecisionValue());
     res += ", label %" + getDefaultDest().getName();
     res += "[\n";
     for (int i = 1; i < getNumCases(); i++) {
