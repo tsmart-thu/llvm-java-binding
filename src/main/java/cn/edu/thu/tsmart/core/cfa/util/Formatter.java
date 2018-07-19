21fb501d8951d048f674089d4736847acc4001d7
cgcgbcbc <cgcgbcbc@163.com>
2018-07-19 15:43:19 +0800

    update copyt wight

--- src/main/java/cn/edu/thu/tsmart/core/cfa/util/Formatter.java
+++ src/main/java/cn/edu/thu/tsmart/core/cfa/util/Formatter.java
@@ -1,21 +1,25 @@
 /*
- * Copyright (c) 2017
- * ------------------
- * Institute on Software System and Engineering
- * School of Software, Tsinghua University
+ * MIT License
  *
- * All Rights Reserved.
+ * Copyright (c) 2018 Institute on Software System and Engineering, School of Software, Tsinghua University
  *
- * NOTICE:
- * All information contained herein is, and remains the property of Tsinghua University.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The intellectual and technical concepts contained herein are proprietary to
- * Tsinghua University and may be covered by China and Foreign Patents, patents in process,
- * and are protected by copyright law.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * Dissemination of this information or reproduction of this material is strictly forbidden
- * unless prior written permission is obtained from Tsinghua University.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package cn.edu.thu.tsmart.core.cfa.util;
 
